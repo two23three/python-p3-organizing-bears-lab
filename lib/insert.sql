12e79b50c856403fb399b9ad8faca1a25741b4c7
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-06-03 15:02:15 +0300

    pass all test

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+  INSERT INTO bears VAlUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+  INSERT INTO bears VAlUES (2, "Rowdy", 10, "M", "black", "intense", 1);
+  INSERT INTO bears VAlUES (3, "Tabitha", 6, "F", "dark brown", "Nice", 1);
+  INSERT INTO bears VAlUES (4, "Sergeant Brown", 19, "M", "Green", "Slimy", 0);
+  INSERT INTO bears VAlUES (5, "Melissa", 13, "F", "dark brown", "goofy", 1);
+  INSERT INTO bears VAlUES (6, "Grinch", 2, "M", "Black", "Grinchy", 1);
+  INSERT INTO bears VAlUES (7, "Wendy", 6, "F", "Blue", "naive", 1);
+  INSERT INTO bears VAlUES (8, null, 20, "M", "black", "aggressive", 0);
